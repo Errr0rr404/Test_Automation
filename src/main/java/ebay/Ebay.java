72930b7d60e8f42cbbf63276e69c6c9801c7da53
mrahman <rahmanww@gmail.com>
2019-09-18 22:55:33 -0400

    selenium

---
+++ src/main/java/ebay/Ebay.java
@@ -0,0 +1,10 @@
+package ebay;
+
+public class Ebay extends CommonAPI_Ebay {
+    public static void main(String[] args) {
+        setup("https://www.ebay.com", "mac", "firefox");
+        sleepFor(5);
+        close();
+    }
+
+}
